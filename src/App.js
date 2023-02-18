1a97926f8730afa611b39bd464bbb6de6091166d
Turóczy Vajk <vajk93@icloud.com>
2023-02-18 21:04:06 +0100

    add chart with chart.css without any functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import Main from "./components/main/Main";
+import Chart from "./components/chart/Chart";
 
-function App(props) {
+function App() {
   return (
     <div>
       <Navbar />
       <Main />
+      <Chart />
     </div>
   );
 }
