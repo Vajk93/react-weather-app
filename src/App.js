91a263f9d95f7934a7fedf9cc8a206bcf93ac514
Turóczy Vajk <vajk93@icloud.com>
2023-02-19 19:50:16 +0100

    fetching data for table: OK

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import Main from "./components/main/Main";
-import Chart from "./components/chart/Chart";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Main />
-      <Chart />
     </div>
   );
 }
--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,8 +1,12 @@
 import "./Main.css";
 import React from "react";
 import { useState } from "react";
+import moment from "moment";
+import "./ForecastTable.css";
 
-function Main(props) {
+// import ForecastTable from "../forecast-table/ForecastTable";
+
+function Main() {
   // budapest coordinates for testing
   // let latitude = 47.53;
   // let longitude = 19.12;
@@ -21,8 +25,8 @@ function Main(props) {
   let [minCelsius, setMinCelsius] = useState("--minCelsius--");
   let [longitude, setLongitude] = useState("--longitude--");
   let [latitude, setLatitude] = useState("--latitude--");
-  let [hours168, setHours168] = useState("--hours168--");
-  let [temps168, setTemps168] = useState("--temps168--");
+  // let [hours168, setHours168] = useState("--hours168--");
+  // let [temps168, setTemps168] = useState("--temps168--");
   let [windDeg, setWindDeg] = useState("--windDeg--");
   let [windSpeed, setWindSpeed] = useState("--windSpeed--");
 
@@ -101,8 +105,105 @@ function Main(props) {
         setWindDeg(fetchedDatas.windDeg);
         setLongitude(fetchedDatas.longitude);
         setLatitude(fetchedDatas.latitude);
+
+        // fetch table datas
+        fetchTableDatas(fetchedDatas.latitude, fetchedDatas.longitude);
       });
   }
+
+  let today = moment().format("dddd");
+  let tomorrow = moment().add(1, "days").format("dddd");
+  let day3 = moment().add(2, "days").format("dddd");
+  let day4 = moment().add(3, "days").format("dddd");
+  let day5 = moment().add(4, "days").format("dddd");
+  let day6 = moment().add(5, "days").format("dddd");
+  let day7 = moment().add(6, "days").format("dddd");
+
+  let [tableRow, setTableRow] = useState("");
+
+  const fetchTableDatas = (latitude, longitude) => {
+    fetch(
+      `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        // itt dolgozhatunk a válaszon, például kiírhatjuk a konzolra
+        console.log(data.hourly.temperature_2m);
+        let hours = [data.hourly.time];
+        let temperatures = [data.hourly.temperature_2m];
+        console.log(hours);
+        console.log(temperatures);
+        let newRow = (
+          <React.Fragment>
+            <tr>
+              <td>22:00</td>
+              <td>{temperatures[0][22]}</td>
+              <td>{temperatures[0][46]}</td>
+              <td>{temperatures[0][70]}</td>
+              <td>{temperatures[0][94]}</td>
+              <td>{temperatures[0][118]}</td>
+              <td>{temperatures[0][142]}</td>
+              <td>{temperatures[0][166]}</td>
+            </tr>
+            <tr>
+              <td>18:00</td>
+              <td>{temperatures[0][18]}</td>
+              <td>{temperatures[0][42]}</td>
+              <td>{temperatures[0][66]}</td>
+              <td>{temperatures[0][90]}</td>
+              <td>{temperatures[0][114]}</td>
+              <td>{temperatures[0][138]}</td>
+              <td>{temperatures[0][162]}</td>
+            </tr>
+            <tr>
+              <td>14:00</td>
+              <td>{temperatures[0][14]}</td>
+              <td>{temperatures[0][38]}</td>
+              <td>{temperatures[0][62]}</td>
+              <td>{temperatures[0][86]}</td>
+              <td>{temperatures[0][110]}</td>
+              <td>{temperatures[0][134]}</td>
+              <td>{temperatures[0][158]}</td>
+            </tr>
+            <tr>
+              <td>10:00</td>
+              <td>{temperatures[0][10]}</td>
+              <td>{temperatures[0][34]}</td>
+              <td>{temperatures[0][58]}</td>
+              <td>{temperatures[0][82]}</td>
+              <td>{temperatures[0][106]}</td>
+              <td>{temperatures[0][130]}</td>
+              <td>{temperatures[0][154]}</td>
+            </tr>
+            <tr>
+              <td>06:00</td>
+              <td>{temperatures[0][6]}</td>
+              <td>{temperatures[0][30]}</td>
+              <td>{temperatures[0][54]}</td>
+              <td>{temperatures[0][78]}</td>
+              <td>{temperatures[0][102]}</td>
+              <td>{temperatures[0][126]}</td>
+              <td>{temperatures[0][150]}</td>
+            </tr>
+            <tr>
+              <td>02:00</td>
+              <td>{temperatures[0][2]}</td>
+              <td>{temperatures[0][26]}</td>
+              <td>{temperatures[0][50]}</td>
+              <td>{temperatures[0][74]}</td>
+              <td>{temperatures[0][98]}</td>
+              <td>{temperatures[0][122]}</td>
+              <td>{temperatures[0][146]}</td>
+            </tr>
+          </React.Fragment>
+        );
+        setTableRow(newRow);
+      })
+      .catch((error) => {
+        // itt dolgozhatunk az esetleges hibával
+        console.error(error);
+      });
+  };
 
   return (
     <React.Fragment>
@@ -175,10 +276,83 @@ function Main(props) {
               <div className="card-body">
                 <h5 className="card-title">Card title</h5>
                 <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
+                <table class="GeneratedTable">
+                  <thead>
+                    <tr>
+                      <th>Hour</th>
+                      <th>{today}</th>
+                      <th>{tomorrow}</th>
+                      <th>{day3}</th>
+                      <th>{day4}</th>
+                      <th>{day5}</th>
+                      <th>{day6}</th>
+                      <th>{day7}</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {tableRow}
+                    {/* <tr>
+                      <td>22:00</td>
+                      <td>Cell</td>
+                      <td>Cell</td>
+                      <td>Cell</td>
+                      <td>Cell</td>
+                      <td>Cell</td>
+                      <td>Cell</td>
+                      <td>Cell</td>
+                    </tr>
+                    <tr>
+                      <td>18:00</td>
+                      <td>Cell</td>
+                      <td>Cell</td>
+                      <td>Cell</td>
+                      <td>Cell</td>
+                      <td>Cell</td>
+                      <td>Cell</td>
+                      <td>Cell</td>
+                    </tr>
+                    <tr>
+                      <td>14:00</td>
+                      <td>Cell</td>
+                      <td>Cell</td>
+                      <td>Cell</td>
+                      <td>Cell</td>
+                      <td>Cell</td>
+                      <td>Cell</td>
+                      <td>Cell</td>
+                    </tr>
+                    <tr>
+                      <td>10:00</td>
+                      <td>Cell</td>
+                      <td>Cell</td>
+                      <td>Cell</td>
+                      <td>Cell</td>
+                      <td>Cell</td>
+                      <td>Cell</td>
+                      <td>Cell</td>
+                    </tr>
+                    <tr>
+                      <td>06:00</td>
+                      <td>Cell</td>
+                      <td>Cell</td>
+                      <td>Cell</td>
+                      <td>Cell</td>
+                      <td>Cell</td>
+                      <td>Cell</td>
+                      <td>Cell</td>
+                    </tr>
+                    <tr>
+                      <td>02:00</td>
+                      <td>Cell</td>
+                      <td>Cell</td>
+                      <td>Cell</td>
+                      <td>Cell</td>
+                      <td>Cell</td>
+                      <td>Cell</td>
+                      <td>Cell</td>
+                    </tr> */}
+                  </tbody>
+                </table>
               </div>
             </div>
           </div>
